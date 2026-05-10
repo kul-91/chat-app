2c9d3f7978ff29b1258fb8a3062d8b83bb9d9a77
Kuldeep <=>
2026-05-10 17:16:44 +0530

    solved production error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.end.MODE === "development" ? " http://localhost:5001/api" : "/api", // backend ka url hoga
+    baseURL: import.meta.env.MODE === "development" ? " http://localhost:5001/api" : "/api", // backend ka url hoga
     withCredentials: true,
 });
